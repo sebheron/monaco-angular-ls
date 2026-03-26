c5a1a6f0ed8efb124f60fda18631770c8f83cca4
sebheron <sebheron21@hotmail.co.uk>
2026-03-26 21:49:13 +0000

    Increase timeout

--- pkg/sandbox/src/main.test.ts
+++ pkg/sandbox/src/main.test.ts
@@ -20,7 +20,7 @@ beforeAll(async () => {
     await vi.waitUntil(() => {
         const m = instance.getTsErrors();
         return m.length ? m : false;
-    }, { timeout: 5000 });
+    }, { timeout: 20000 });
     instance.tsEditor.setValue('');
     await vi.waitUntil(() => {
         const m = instance.getTsErrors();
@@ -36,8 +36,7 @@ afterEach(async () => {
     });
 });
 
-//Skipping as done, note remove this
-describe.skip('NG Declaration Errors', () => {
+describe('NG Declaration Errors', () => {
     test('NG1001 DECORATOR_ARG_NOT_LITERAL', async () => {
         instance.tsEditor.setValue(
             `import { Component } from '@angular/core';
